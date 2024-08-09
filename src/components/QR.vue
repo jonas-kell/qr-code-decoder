e6404b2e9b7fc087f9c5c5b82d6b24bc200be55f
Jonas <j.kell@liwetec.com>
2024-08-09 22:59:42 +0200

    feat: generate qr code

---
+++ src/components/QR.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+    import QrcodeVue from "qrcode.vue";
+    import { VTextField, VRow } from "vuetify/components";
+    import { ref } from "vue";
+
+    const value = ref("https://example.com");
+    const size = ref(400);
+</script>
+
+<template>
+    <div class="container">
+        <v-text-field v-model="value"></v-text-field>
+        <v-row justify="center">
+            <qrcode-vue :value="value" :size="size" level="H" id="qr" />
+        </v-row>
+    </div>
+</template>
+
+<style>
+    #qr {
+        width: 80% !important;
+        height: 80% !important;
+
+        max-height: 85vh;
+        max-width: 85vh;
+    }
+</style>
--- src/router.ts
+++ src/router.ts
@@ -1,8 +1,12 @@
 import { createWebHashHistory, createRouter } from "vue-router";
 
 import Index from "./components/Index.vue";
+import QR from "./components/QR.vue";
 
-const routes = [{ path: "/", component: Index, name: "index" }];
+const routes = [
+    { path: "/", component: Index, name: "index" },
+    { path: "/generate", component: QR, name: "generate" },
+];
 
 export default createRouter({
     history: createWebHashHistory(),
